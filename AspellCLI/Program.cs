b726676d93039622be15eb5eddfb9229a9f11924
hunterlan <ksharykin@sysonline.com>
2021-06-16 14:35:44 +0300

    Added command line parser

---
+++ AspellCLI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+
+namespace AspellCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .WithParsed(RunOptions);
+        }
+        
+        static void RunOptions(CommandLineOptions opts)
+        {
+            var gotFiles = opts.Files;
+        }
+    }
+
+    class CommandLineOptions
+    {
+        [Option('f', "files", Required = true, HelpText = "Input files, which should be checked")]
+        public List<string> Files { get; set; }
+    }
+}
