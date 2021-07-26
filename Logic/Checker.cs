ea02eb16afce7285e686ca6a51b4d25ca64ed2b5
Konstantin Sharykin <kotyshe@gmail.com>
2021-07-26 15:22:50 +0300

    Wrote functions loading rules and source code. Wrote function, which parse source code.

--- Logic/Checker.cs
+++ Logic/Checker.cs
@@ -1,19 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Models;
 
 namespace Logic
 {
     public class Checker
     {
+        private List<Rule> Rules;
+        private readonly string _pathToRules; 
+
+        public Checker()
+        {
+            _pathToRules = "rules.json";
+            LoadRules();
+        }
         public List<InfoFile> CheckFiles(List<string> fileNames, List<string> ruleIgnore)
         {
-            // TODO: Start use default rules
             // TODO: Use Hunspell and ignore words which in rule ignore
+            foreach (var fileName in fileNames)
+            {
+                string sourceCode = LoadFile(fileName);
+                if (sourceCode == null)
+                {
+                    Console.WriteLine("File cannot be load");
+                }
+                else
+                {
+                    var parsedSourceCode = ParseSourceCode(sourceCode);
+                    var words = parsedSourceCode.Split(' ');
+                }
+            }
             throw new NotImplementedException();
+        }
+
+        private async Task LoadRules()
+        {
+            await using var fs = new FileStream(_pathToRules, FileMode.Open);
+            Rules = await JsonSerializer.DeserializeAsync<List<Rule>>(fs);
         }
         
         // TODO: Write reading of file
-        // TODO: Write parsing special symbols
+        private string LoadFile(string pathToFile)
+        {
+            var fileData = string.Empty;
+            try
+            {
+                using (StreamReader sr = new StreamReader(pathToFile))
+                {
+                    fileData = sr.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
+            return fileData;
+        }
+        
+        // TODO: Write parsing special symbols from file
+        private string ParseSourceCode(string sourceCode)
+        {
+            var temp = sourceCode;
+            temp = temp.Replace("/*", " ").Replace('*', ' ').Replace("*/", " ");
+
+            return temp;
+        }
     }
 }
---
+++ Models/Rule.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Models
+{
+    public class Rule
+    {
+        [JsonPropertyName("typefile")]
+        public List<string> TypeFile { get; init; }
+        
+        [JsonPropertyName("typefile")]
+        public List<string> Comments { get; init; }
+    }
+}
