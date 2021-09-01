ba32927c79d70d8ba5acb1bdf052cc0997e05424
Konstantin Sharykin <kotyshe@gmail.com>
2021-09-01 11:54:06 +0300

    Implement dependency inversion

--- AspellCLI/Program.cs
+++ AspellCLI/Program.cs
@@ -11,11 +11,15 @@ namespace AspellCLI
     class Program
     {
         private static List<string> _filesToCheck = new();
-        private static bool _isHtmlMode = false;
+        private static bool _isHtmlMode;
         private static List<string> _rulesForIgnore = new();
-        private static readonly IChecker _checker = new Checker(); 
-        
-        static Program() => Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        private static readonly IChecker Checker;
+
+        static Program()
+        {
+            Checker = CheckerFactory.GetCheckerObject();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
 
         private static void Main(string[] args)
         {
@@ -26,8 +30,10 @@ namespace AspellCLI
         private static void RunOptions(CommandLineOptions opts)
         {
             _filesToCheck = opts.Files.ToList();
+            _isHtmlMode = opts.IsHtml;
             if (!string.IsNullOrWhiteSpace(opts.RulesForIgnore)) _rulesForIgnore.Add(opts.RulesForIgnore);
-            var result = _checker.CheckFiles(_filesToCheck, _rulesForIgnore);
+            
+            var result = Checker.CheckFiles(_filesToCheck, _rulesForIgnore);
             ShowResult(result);
         }
 
@@ -52,7 +58,7 @@ namespace AspellCLI
         [Option('f', "files", Required = true, HelpText = "Input files, which should be checked")]
         public IEnumerable<string> Files { get; set; }
         
-        [Option("isHtml", Required = false, HelpText = "Is output result will be in HTML. If not, will be displayed in CLI. Default - false")]
+        [Option("isHtml", Required = false, Default = false, HelpText = "Is output result will be in HTML. If not, will be displayed in CLI. Default - false")]
         public bool IsHtml { get; set; } 
         
         [Option("rules-for-ignore", Required = false, HelpText = "Input file, from which will take words to ignore", Default = "")]
---
+++ Logic/CheckerFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace Logic
+{
+    public class CheckerFactory
+    {
+        public static IChecker GetCheckerObject()
+        {
+            return new Checker();
+        }
+    }
+}
