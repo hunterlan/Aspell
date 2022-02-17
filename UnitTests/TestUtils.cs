b0b5063f4c2d92ff10632bc07a3686de103bd825
Konstantin Sharykin <kotyshe@gmail.com>
2022-02-17 11:44:35 +0200

    Create unit test project

---
+++ UnitTests/TestUtils.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class TestUtils
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public void Test()
+        {
+            
+        }
+    }
+}
